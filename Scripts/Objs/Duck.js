aa7bfa8860f5086a81b9548127c3c179d9ca6411
Pablo2173 <pablobernal17@gmail.com>
2026-02-17 10:20:06 +0100

    c1 Patos basics #5
    Organizando el juego y empezando el movimiento del pato

--- Scripts/Objs/Duck.js
+++ Scripts/Objs/Duck.js
@@ -6,15 +6,34 @@ const StatusDuck = {
 };
 
 class Duck {
-    constructor() { // `constructor` es una palabra reservada
-        this._speed = null;
+    constructor(element) { // `constructor` es una palabra reservada
+        super(element, 200, 200);
+        this._speed = 5;
         this._weapon = null;
         this.dmgMult = 1;
         this.effMult = 1;
         this._state = StatusDuck.MAIN;
     }
 
-    mostrarNombre() {
-        console.log(`Mi nombre es ${this._nombre}`);
+    //Movimiento del pato
+    move(direction) {
+        if (this._state == StatusDuck.STUNNED) return;
+        switch (direction) {
+            case "up":
+                this.y -= this._speed;
+                break;
+            case "down":
+                this.y += this._speed;
+                break;
+            case "left":
+                this.x -= this._speed;
+                break;
+            case "right":
+                this.x += this._speed;
+                break;
+        }
+        this.render();
     }
+
+
 }
--- Scripts/Objs/GameItem.js
+++ Scripts/Objs/GameItem.js
@@ -1,9 +1,14 @@
-class Enemy {
-    constructor() { // `constructor` es una palabra reservada
-        this._graphic = null;
-        this._position = { x: null, y: null };
-        this._hp = null;
-        this._angle = null;
+class GameItem {
+    constructor(element, x = 0, y = 0) { // `constructor` es una palabra reservada
+        this.x = x;
+        this.y = y;
+        this.element = element;
+        this.render();
     }
 
+    // Renderiza la posición en pantalla
+    render() {
+        this.element.style.left = this.x + "px";
+        this.element.style.top = this.y + "px";
+    }
 }
