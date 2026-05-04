469a16e97ff25be4b68e9bb55618fb6d3ae88839
rodrimartineez <rodrim10@ucm.es>
2026-05-04 22:06:17 +0200

    falta ataque a distancia

---
+++ src/GameObjects/Projectiles/bubblesCroco.js
@@ -0,0 +1,17 @@
+import Projectile from './projectile.js';
+import balaTexture from '../../../assets/sprites/Projectiles/bubbles_croco.png';
+
+export default class Bala extends Projectile {
+    constructor(scene, x, y, config = {}) {
+        super(scene, x, y, 'bubbles_croco', config);
+        this.setScale(5);
+        // Ajustar el tamaño de colisión al tamaño visual del sprite escalado
+        if (this.body) {
+            this.body.setSize(this.width / 6, this.height / 6);
+        }
+    }
+
+    static preload(scene) {
+        scene.load.image('bubbles_croco', balaTexture);
+    }
+}
